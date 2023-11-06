cef5b4c686ad89e50ab82b1719a5a8d8d065e731
i888844 <i888844@yandex.ru>
2023-11-07 00:06:41 +0300

    2nd commit

--- Solving Geometric Tasks.cpp
+++ Solving Geometric Tasks.cpp
@@ -10,32 +10,42 @@
 
 #pragma warning(disable: 4996)
 
+#define ESC 27
+#define PLUS 61
+#define MINUS 45
+#define UP 72
+#define DOWN 80
+#define LEFT 75
+#define RIGHT 77
+
 typedef struct
 {
     int x;
     int y;
 } dot;
 
-HDC hDC;
-HWND hWnd;
+HWND hWnd = GetConsoleWindow();
+HDC hDC = GetDC(hWnd);
 RECT Rect;
 HBRUSH hBrush;
 HBRUSH hOldBrush;
-HBRUSH hBrushBlack = CreateSolidBrush(RGB(0, 0, 0));
 HBRUSH hBrushWhite = CreateSolidBrush(RGB(255, 255, 255));
-HBRUSH hBrushRed = CreateSolidBrush(RGB(255, 0, 0));
-HBRUSH hBrushGrey = CreateSolidBrush(RGB(71, 74, 81));
+HPEN hPenBlack = CreatePen(PS_SOLID, 5, RGB(0, 0, 0));
+HPEN hPenRed = CreatePen(PS_SOLID, 5, RGB(255, 0, 0));
+HPEN hPenGreen = CreatePen(PS_SOLID, 5, RGB(0, 255, 0));
+HPEN hPenBlue = CreatePen(PS_SOLID, 5, RGB(0, 0, 255));
+HPEN hPenGrey = CreatePen(PS_SOLID, 3, RGB(71, 74, 81));
 HFONT hFont;
 HFONT hOldFont;
 LOGFONT Lf = { 0 };
 
-bool is_square_and_sides_not_zero(dot* rect_dots)
+bool is_square_and_sides_not_zero(dot* square_dots)
 {
     bool result = false;
-    int left_side = sqrt(pow(rect_dots[3].x - rect_dots[0].x, 2) + pow(rect_dots[3].y - rect_dots[0].y, 2));
-    int right_side = sqrt(pow(rect_dots[2].x - rect_dots[1].x, 2) + pow(rect_dots[2].y - rect_dots[1].y, 2));
-    int top_side = sqrt(pow(rect_dots[1].x - rect_dots[0].x, 2) + pow(rect_dots[1].y - rect_dots[0].y, 2));
-    int bottom_side = sqrt(pow(rect_dots[2].x - rect_dots[3].x, 2) + pow(rect_dots[2].y - rect_dots[3].y, 2));
+    int left_side = sqrt(pow(square_dots[3].x - square_dots[0].x, 2) + pow(square_dots[3].y - square_dots[0].y, 2));
+    int right_side = sqrt(pow(square_dots[2].x - square_dots[1].x, 2) + pow(square_dots[2].y - square_dots[1].y, 2));
+    int top_side = sqrt(pow(square_dots[1].x - square_dots[0].x, 2) + pow(square_dots[1].y - square_dots[0].y, 2));
+    int bottom_side = sqrt(pow(square_dots[2].x - square_dots[3].x, 2) + pow(square_dots[2].y - square_dots[3].y, 2));
     if (left_side != 0 && left_side == right_side && top_side != 0 && top_side == bottom_side)
     {
         result = true;
@@ -43,22 +53,183 @@ bool is_square_and_sides_not_zero(dot* rect_dots)
     return result;
 }
 
-bool sides_parallel_to_axes(dot* rect_dots)
+bool sides_parallel_to_axes(dot* square_dots)
 {
     bool result = false;
-    if (rect_dots[0].x == rect_dots[3].x && rect_dots[1].x == rect_dots[2].x && rect_dots[0].y == rect_dots[1].y && rect_dots[3].y == rect_dots[2].y)
+    if (square_dots[0].x == square_dots[3].x && square_dots[1].x == square_dots[2].x && square_dots[0].y == square_dots[1].y && square_dots[3].y == square_dots[2].y)
     {
         result = true;
     }
     return result;
 }
 
+void create_dots_rand(dot* dots)
+{
+    srand(time(NULL));
+    for (int i = 0; i < 10; i++)
+    {
+        dots[i].x = rand() % 1001 - 500;
+        dots[i].y = rand() % 1001 - 500;
+    }
+}
+
+void create_dots_from_keyboard(dot* dots)
+{
+    printf("Введите точки:\n");
+    for (int i = 0; i < 10; i++)
+    {
+        printf("Введите X для точки №%i: ", i + 1);
+        scanf_s("%i", &dots[i].x);
+        printf("Введите Y для точки №%i: ", i + 1);
+        scanf_s("%i", &dots[i].y);
+    }
+}
+
+void create_dots_from_file(dot* dots, const char* filename)
+{
+    FILE* file = fopen(filename, "r");
+    if (file == NULL)
+    {
+        printf("[Ошибка]: Не удалось открыть файл.\n");
+        exit(1);
+    }
+    int number = 0;
+    srand(time(NULL));
+    for (int i = 0; i < 20; i++)
+    {
+        number = 0;
+        if (fscanf(file, "%i", &number) == 1)
+        {
+            if (i % 2)
+            {
+                dots[i].x = number;
+            }
+            else
+            {
+                dots[i].y = number;
+            }
+        }
+        else
+        {
+            if (i % 2)
+            {
+                dots[i].x = rand() % 1001 - 500;
+            }
+            else
+            {
+                dots[i].y = rand() % 1001 - 500;
+            }
+        }
+    }
+}
+
+bool in_square(dot* square_dots, dot* dot)
+{
+    bool result = false;
+    if (dot->x >= square_dots[0].x && dot->x <= square_dots[2].x && dot->y <= square_dots[0].y && dot->y >= square_dots[2].y)
+    {
+        result = true;
+    }
+    return result;
+}
+
+void print_axes(int x_start, int x_end, int y_start, int y_end, int delta)
+{
+    int i = 0;
+    int coord = 0;
+    int horizontal_center = x_end / 2;
+    int vertical_center = y_end / 2;
+    CHAR S[10];
+    SelectObject(hDC, hBrushWhite);
+    Rectangle(hDC, 0, 0, x_end, y_end);
+    SelectObject(hDC, hPenBlack);
+    MoveToEx(hDC, horizontal_center, Rect.top, NULL);
+    LineTo(hDC, horizontal_center, Rect.bottom);
+    TextOut(hDC, x_end - 16, vertical_center, (LPCSTR)"X", strlen("X"));
+    TextOut(hDC, horizontal_center, vertical_center, (LPCSTR)"0", strlen("0"));
+    MoveToEx(hDC, 0, vertical_center, NULL);
+    LineTo(hDC, x_end, vertical_center);
+    TextOut(hDC, horizontal_center, y_start, (LPCSTR)"Y", strlen("Y"));
+    SelectObject(hDC, hPenGrey);
+    for (i = horizontal_center + delta; i < x_end; i += delta)
+    {
+        MoveToEx(hDC, i, y_start, NULL);
+        LineTo(hDC, i, y_end);
+        coord += delta;
+        sprintf_s(S, "%i", coord);
+        TextOut(hDC, i, vertical_center, (LPCSTR)S, strlen(S));
+    }
+    coord = 0;
+    for (i = horizontal_center - delta; i > x_start; i -= delta)
+    {
+        MoveToEx(hDC, i, y_start, NULL);
+        LineTo(hDC, i, y_end);
+        coord += delta;
+        sprintf_s(S, "%i", -1 * coord);
+        TextOut(hDC, i, vertical_center, (LPCSTR)S, strlen(S));
+    }
+    coord = 0;
+    for (i = vertical_center - delta; i > y_start; i -= delta)
+    {
+        MoveToEx(hDC, x_start, i, NULL);
+        LineTo(hDC, x_end, i);
+        coord += delta;
+        sprintf_s(S, "%i", coord);
+        TextOut(hDC, horizontal_center, i, (LPCSTR)S, strlen(S));
+    }
+    coord = 0;
+    for (i = vertical_center + delta; i < y_end; i += delta)
+    {
+        MoveToEx(hDC, x_start, i, NULL);
+        LineTo(hDC, x_end, i);
+        coord += delta;
+        sprintf_s(S, "%i", -1 * coord);
+        TextOut(hDC, horizontal_center, i, (LPCSTR)S, strlen(S));
+    }
+}
+
+void print_square(dot* square_dots, int x_end, int y_end)
+{
+    int horizontal_center = x_end / 2;
+    int vertical_center = y_end / 2;
+    SelectObject(hDC, hPenRed);
+    MoveToEx(hDC, horizontal_center + square_dots[0].x, vertical_center - square_dots[0].y, NULL);
+    LineTo(hDC, horizontal_center + square_dots[1].x, vertical_center - square_dots[1].y);
+    LineTo(hDC, horizontal_center + square_dots[2].x, vertical_center - square_dots[2].y);
+    LineTo(hDC, horizontal_center + square_dots[3].x, vertical_center - square_dots[3].y);
+    LineTo(hDC, horizontal_center + square_dots[0].x, vertical_center - square_dots[0].y);
+}
+
+void print_dots(dot* square_dots, dot* dots, int x_end, int y_end)
+{
+    int horizontal_center = x_end / 2;
+    int vertical_center = y_end / 2;
+    for (int i = 0; i < 10; i++)
+    {
+        if (in_square(square_dots, &dots[i]))
+        {
+            SelectObject(hDC, hPenGreen);
+        }
+        else
+        {
+            SelectObject(hDC, hPenBlue);
+        }
+        Ellipse(hDC, horizontal_center + (dots[i].x - 3), vertical_center - (dots[i].y), horizontal_center + (dots[i].x + 3), vertical_center - (dots[i].y + 3));
+    }
+}
+
+void clear_screen()
+{
+    SelectObject(hDC, hBrushWhite);
+    Rectangle(hDC, 0, 0, 4000, 2200);
+}
+
 int main()
 {
     setlocale(LC_ALL, "Russian");
-    Lf.lfHeight = 32;
+    Lf.lfHeight = 16;
     Lf.lfWeight = 900;
-    Lf.lfWidth = 16;
+    Lf.lfWidth = 8;
     Lf.lfCharSet = RUSSIAN_CHARSET;
     strcpy((char*)Lf.lfFaceName, "Times New Roman");
     hFont = CreateFontIndirect(&Lf);
@@ -66,25 +237,142 @@ int main()
     SetTextColor(hDC, RGB(0, 0, 0));
     SetBkColor(hDC, RGB(255, 255, 255));
     GetClientRect(hWnd, &Rect);
-    dot rect_dots[4];
+    dot square_dots[4];
     printf("Введите точки углов квадрата:\n");
     for (int i = 0; i < 4; i++)
     {
         printf("Введите X для точки №%i: ", i + 1);
-        scanf_s("%i", &rect_dots[i].x);
+        scanf_s("%i", &square_dots[i].x);
         printf("Введите Y для точки №%i: ", i + 1);
-        scanf_s("%i", &rect_dots[i].y);
+        scanf_s("%i", &square_dots[i].y);
     }
-    while (!is_square_and_sides_not_zero(&rect_dots[0]) || !sides_parallel_to_axes(&rect_dots[0]))
+    while (!is_square_and_sides_not_zero(&square_dots[0]) || !sides_parallel_to_axes(&square_dots[0]))
     {
-        printf("[Ошибка]: введённые координаты не образуют квадрат, стороны которого параллельны осям. Повторите попытку:");
+        printf("[Ошибка]: введённые координаты не образуют квадрат, стороны которого параллельны осям. Повторите попытку:\n");
         for (int i = 0; i < 4; i++)
         {
-            printf("\nВведите X для точки №%i: ", i + 1);
-            scanf_s("%i", &rect_dots[i].x);
-            printf("\nВведите Y для точки №%i: ", i + 1);
-            scanf_s("%i", &rect_dots[i].y);
+            printf("Введите X для точки №%i: ", i + 1);
+            scanf_s("%i", &square_dots[i].x);
+            printf("Введите Y для точки №%i: ", i + 1);
+            scanf_s("%i", &square_dots[i].y);
         }
     }
+    int cmd = 0;
+    dot dots[10];
+    printf("Выберите вариант формирования точек:\n");
+    printf("1. Сформировать точки случайным образом.\n");
+    printf("2. Сформировать точки вводом с клавиатуры.\n");
+    printf("3. Сформировать точки данными из файла.\n");
+    printf("0. Завершить работу программы.\n");
+    printf("Введите номер действия: ");
+    scanf_s("%i", &cmd);
+    while (cmd < 0 || cmd > 3)
+    {
+        printf("[Ошибка]: выбран неверный номер действия. Введите номер действия: ");
+        scanf_s("%i", &cmd);
+    }
+    switch (cmd)
+    {
+        case 0:
+        {
+            return 0;
+            break;
+        }
+        case 1:
+        {
+            create_dots_rand(&dots[0]);
+            break;
+        }
+        case 2:
+        {
+            create_dots_from_keyboard(&dots[0]);
+            break;
+        }
+        case 3:
+        {
+            char filename[256];
+            printf("Введите название файла с форматом (пример: filename.txt): ");
+            scanf_s("%255s", &filename, sizeof(filename));
+            create_dots_from_file(&dots[0], filename);
+            break;
+        }
+    }
+
+    for (int i = 0; i < 10; i++)
+    {
+        if (in_square(&square_dots[0], &dots[i]))
+        {
+            printf("Точка №%i - внутри квадрата:\n", i + 1);
+        }
+        else
+        {
+            printf("Точка №%i - вне квадрата:\n", i + 1);
+        }
+        printf("X: %i\n", dots[i].x);
+        printf("Y: %i\n\n", dots[i].y);
+    }
+    int x_start = Rect.left;
+    int x_end = Rect.right;
+    int y_start = Rect.top;
+    int y_end = Rect.bottom;
+    int delta = 50;
+    int key = 0;
+    do
+    {
+        key = _getch();
+        switch (key)
+        {
+            case PLUS:
+            {
+                x_start += 50;
+                x_end -= 50;
+                y_start += 50;
+                y_end -= 50;
+                delta += 50;
+                break;
+            }
+            case MINUS:
+            {
+                if ((delta - 50) <= 0)
+                {
+                    x_start -= 50;
+                    x_end += 50;
+                    y_start -= 50;
+                    y_end += 50;
+                    delta -= 50;
+                }
+                break;
+            }
+            case UP:
+            {
+                y_start -= 50;
+                y_end -= 50;
+                break;
+            }
+            case DOWN:
+            {
+                y_start += 50;
+                y_end += 50;
+                break;
+            }
+            case LEFT:
+            {
+                x_start -= 100;
+                x_end -= 100;
+                break;
+            }
+            case RIGHT:
+            {
+                x_start += 100;
+                x_end += 100;
+                break;
+            }
+        }
+        clear_screen();
+        print_axes(x_start, x_end, y_start, y_end, delta);
+        print_square(&square_dots[0], x_end, y_end);
+        print_dots(&square_dots[0], &dots[0], x_end, y_end);
+    } while (key != ESC);
+    _getch();
     return 0;
 }
